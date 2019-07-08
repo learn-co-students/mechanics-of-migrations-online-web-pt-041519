5ff72607ff25283755aab5019c35abaa631093ad
aparkening <aaron.parkening@gmail.com>
2019-07-08 14:52:52 -0400

    Done. Add Active Record migrations.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,19 @@
+class CreateArtists < ActiveRecord::Migration[5.1]
+  # Do
+  def up
+  end
+ 
+  # Undo
+  def down
+  end
+
+  # Write migration to db
+	def change
+	  create_table :artists do |t|
+	    t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+	  end
+	end
+end
