01d05eb7f23be18e677d002a90be33e768706a9d
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-11 03:56:54 +0000

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,11 +1,5 @@
 class CreateArtists < ActiveRecord::Migration[4.2]
 
-  def up #to execute when migrtion is run => do
-  end
-
-  def down #to execute when migrstion is rolled back => undo
-  end
-
   def change
     create_table :artists do |t|
       t.string :name
@@ -14,5 +8,4 @@ class CreateArtists < ActiveRecord::Migration[4.2]
       t.string :hometown
     end
   end
-
 end
